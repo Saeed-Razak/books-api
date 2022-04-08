26f1212dc0fb16200f6c310697eb369e5026089a
Saeed-Razak <saeedabdulrazak95@gmail>
2022-04-08 14:34:17 +0000

    first commit

---
+++ books.js
@@ -0,0 +1,26 @@
+const books = [
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:" FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"}, 
+     {title:"THINGS FALL APART", author:"Chinua Achebe", publication:"05/05/1995",type:"Tropic"},
+]
+
+
+     module.exports=books
---
+++ controllers/booksController.js
@@ -0,0 +1,30 @@
+const books = require("../books")
+
+//get all books
+
+const getBooks =(req, res)=>{
+    res.status(200).json(books)
+}
+
+//delete a book
+/*const deleteBook = async(req, res)=>{
+    const foundBook = await books.findbyTitle(req, params.title)
+    if(foundBook){
+        foundBook.remove()
+        res.json({msg:"book removed"})
+
+    } else{
+        res.status(404).json({error:"Book not found"})
+    }
+
+
+}*/
+//delete a book
+const deleteBook = (req, res)=>{
+    res.json(books.filter((book)=>books.title!==req.params.name))
+
+}
+
+
+
+module.exports={getBooks, deleteBook}
